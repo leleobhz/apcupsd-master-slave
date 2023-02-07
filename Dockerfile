80a581836bf1736c1391d41711b8dfac1ce10f4a
Scott Ueland <41088895+bnhf@users.noreply.github.com>
2023-02-07 12:12:57 -0700

    Add env variables and persistent bind mount data

--- Dockerfile
+++ Dockerfile
@@ -1,21 +1,17 @@
 FROM ubuntu:latest
 LABEL Greg Ewing (https://github.com/gregewing)
 ENV LANG=C.UTF-8 DEBIAN_FRONTEND=noninteractive
-# ENV TZ=Europe/London
-
-COPY scripts /usr/local/bin
-COPY start.sh /
 
 RUN echo Starting. \
  && apt-get -q -y update \
  && apt-get -q -y install --no-install-recommends apcupsd dbus libapparmor1 libdbus-1-3 libexpat1 tzdata \
  && apt-get -q -y full-upgrade \
  && rm -rif /var/lib/apt/lists/* \
- && rm -rf /etc/apcupsd/* \
-# && mv /usr/local/bin/apcupsd         /etc/default/apcupsd \
-# && mv /usr/local/bin/apcupsd.conf    /etc/apcupsd/apcupsd.conf \
-# && mv /usr/local/bin/hosts.conf      /etc/apcupsd/hosts.conf \
-# && mv /usr/local/bin/doshutdown      /etc/apcupsd/doshutdown \
+ && mkdir /opt/apcupsd \
+ && mv /etc/apcupsd/* /opt/apcupsd \
  && echo Finished.
+
+COPY scripts /opt/apcupsd
+COPY start.sh /
 
 CMD /start.sh
--- start.sh
+++ start.sh
@@ -3,75 +3,33 @@
 # Config file moves transferred from Dockerfile to support
 # binding /etc/apcupsd to user-specified host directory
 
-# Check if apcupsd.conf already exists
-if [ ! -f /etc/apcupsd/apcupsd.conf ]; then
-  mv /usr/local/bin/apcupsd /etc/default/apcupsd \
-  && mv /usr/local/bin/apcupsd.conf /etc/apcupsd/apcupsd.conf \
-  && echo "No existing apcupsd.conf found"
-  else
-  mv /usr/local/bin/apcupsd /etc/default/apcupsd \
-  && rm /usr/local/bin/apcupsd.conf \
-  && echo "Existing apcupsd.conf found, and will be used"
-fi
+cp /opt/apcupsd/apcupsd /etc/default/apcupsd
 
-# Check if hosts.conf already exists
-if [ ! -f /etc/apcupsd/hosts.conf ]; then
-  mv /usr/local/bin/hosts.conf /etc/apcupsd/hosts.conf \
-  && echo "No existing hosts.conf found"
-else
-  rm /usr/local/bin/hosts.conf \
-  && echo "Existing hosts.conf found, and will be used"
-fi
+# Check if /etc/apcupsd files exist, and copy them from /opt/apcupsd if they don't
+files=( apcupsd.conf hosts.conf doshutdown apccontrol changeme commfailure commok killpower multimon.conf offbattery onbattery ups-monitor )
 
-# Check if doshutdown already exists
-if [ ! -f /etc/apcupsd/doshutdown ]; then
-  mv /usr/local/bin/doshutdown /etc/apcupsd/doshutdown \
-  && echo "No existing doshutdown found"
-else
-  rm /usr/local/bin/doshutdown \
-  && echo "Existing doshutdown found, and will be used"
-fi
+for i in "${files[@]}"
+  do
+    if [ ! -f /etc/apcupsd/$i ]; then
+      cp /opt/apcupsd/$i /etc/apcupsd/$i \
+      && echo "No existing $i found"
+    else
+      echo "Existing $i found, and will be used"
+    fi
+  done
 
-# Check if UPSNAME environment variable is set, and if so update apcupsd.conf
-if [ ! -z $UPSNAME ]; then
-  sed -i 's|^UPSNAME.*|UPSNAME '"$UPSNAME"'|' /etc/apcupsd/apcupsd.conf \
-  && echo "UPSNAME set to: \"$UPSNAME\""
-fi
+# First, add an extra # to the second UPSNAME used for EPROM updates
+sed -i 's/^#UPSNAME UPS_IDEN/##UPSNAME UPS_IDEN/' /etc/apcupsd/apcupsd.conf
 
-# Check if UPSCABLE environment variable is set, and if so update apcupsd.conf
-if [ ! -z $UPSCABLE ]; then
-  sed -i 's|^UPSCABLE.*|UPSCABLE '"$UPSCABLE"'|' /etc/apcupsd/apcupsd.conf \
-  && echo "UPSCABLE set to: \"$UPSCABLE\""
-fi
+# Check if environment variables are set, and if so update apcupsd.conf
+settings=( "UPSNAME" "UPSCABLE" "UPSTYPE" "DEVICE" "POLLTIME" "NETSERVER" "NISIP" "NISPORT" "ONBATTERYDELAY" "BATTERYLEVEL" "MINUTES" "TIMEOUT" "SELFTEST" )
 
-# Check if UPSTYPE environment variable is set, and if so update apcupsd.conf
-if [ ! -z $UPSTYPE ]; then
-  sed -i 's|^UPSTYPE.*|UPSTYPE '"$UPSTYPE"'|' /etc/apcupsd/apcupsd.conf \
-  && echo "UPSTYPE set to: \"$UPSTYPE\""
-fi
-
-# Check if DEVICE environment variable is set, and if so update apcupsd.conf
-if [ ! -z $DEVICE ]; then
-  sed -i 's|^#DEVICE /dev/tty0.*|DEVICE '"$DEVICE"'|' /etc/apcupsd/apcupsd.conf \
-  && echo "DEVICE set to: \"$DEVICE\""
-fi
-
-# Check if NETSERVER environment variable is set, and if so update apcupsd.conf
-if [ ! -z $NETSERVER ]; then
-  sed -i 's|^NETSERVER.*|NETSERVER '"$NETSERVER"'|' /etc/apcupsd/apcupsd.conf \
-  && echo "NETSERVER set to: \"$NETSERVER\""
-fi
-
-# Check if NISIP environment variable is set, and if so update apcupsd.conf
-if [ ! -z $NISIP ]; then
-  sed -i 's|^NISIP.*|NISIP '"$NISIP"'|' /etc/apcupsd/apcupsd.conf \
-  && echo "NISIP set to: \"$NISIP\""
-fi
-
-# Check if NISPORT environment variable is set, and if so update apcupsd.conf
-if [ ! -z $NISPORT ]; then
-  sed -i 's|^NISPORT.*|NISPORT '"$NISPORT"'|' /etc/apcupsd/apcupsd.conf \
-  && echo "NISPORT set to: \"$NISPORT\""
-fi
+for i in ${settings[@]}
+  do
+    if [ ! -z ${!i} ]; then
+      sed -i -r 's/(^'"$i"'.*|^#'"$i"'.*)/'"$i"' '"${!i}"'/' /etc/apcupsd/apcupsd.conf \
+      && awk '$1 ~ /^'"$i"'/' /etc/apcupsd/apcupsd.conf
+    fi
+  done
 
 /sbin/apcupsd -b
